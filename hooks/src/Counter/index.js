21c61beb8a52f0fcac72e7eca877430d360a8ce8
valours <leflem.valentin@gmail.com>
2019-02-28 02:44:21 +0100

    add hook counter

--- hooks/src/Counter/index.js
+++ hooks/src/Counter/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Counter = () => (
-  <div>
-    <h1>Counter</h1>
-  </div>
-)
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  
+  return (
+    <div>
+      <h1>Counter</h1>
+      <div>Count : {count}</div>
+      <button onClick={() => setCount(count + 1)}>up</button>
+    </div>
+  )
+}
 
 export default Counter;
